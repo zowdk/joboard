541436bd7344d423e1927a4b3016b057228b8261
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2024-02-18 13:08:09 +0100

    get multiple documents from firestore db

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -8,18 +8,24 @@ import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import JobCard from "./components/JobCard";
 import jobData from "./JobDummyData";
-import { useState } from "react";
-import { collection,query, where, getDocs } from "firebase/firestore";
-import { db } from "./firebase.config"
-
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "./firebase.config";
 
 function App() {
   const [jobs, setJobs] = useState([]);
 
-  const fetchJobs = asynch() => {
+  const fetchJobs = async () => {
     //import db from firestore
     const q = query(collection(db, "jobs"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      //doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
   };
+  useEffect(() => {
+    fetchJobs();
+  }, []);
 
   return (
     <div>
