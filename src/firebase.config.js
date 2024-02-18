43a45b696d2dd19a9af25d21e78737bb826f9bf8
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2024-02-18 12:32:20 +0100

    initialize firebase

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAAYSgAhX2MPAPUy3CRayoQcw3WW3AaIE8",
+  authDomain: "online-job-portal-49f41.firebaseapp.com",
+  projectId: "online-job-portal-49f41",
+  storageBucket: "online-job-portal-49f41.appspot.com",
+  messagingSenderId: "871978456873",
+  appId: "1:871978456873:web:34d253e212d99c4cd14938",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
