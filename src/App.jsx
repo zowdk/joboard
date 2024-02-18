9cec0fa9e664480922cc856caa1e40bd1de4ea8b
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2024-02-18 12:59:13 +0100

    import db from firestore

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,19 @@ import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import JobCard from "./components/JobCard";
 import jobData from "./JobDummyData";
+import { useState } from "react";
+import { collection,query, where, getDocs } from "firebase/firestore";
+import { db } from "./firebase.config"
+
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = asynch() => {
+    //import db from firestore
+    const q = query(collection(db, "jobs"));
+  };
+
   return (
     <div>
       <Navbar />
